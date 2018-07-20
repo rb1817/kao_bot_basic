63dffcb6bfc5f07cc0d0b0a8fcba2dae5ecfdbce
Leedonggyu <rb2978@naver.com>
2018-07-20 01:46:34 +0000

    read

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,59 +1,64 @@
 class KakaoController < ApplicationController
   def keyboard #json를 hash로,: =>,  :둘다가능
+
     @keyboard=
     {
       :type => "buttons",
-      buttons: ["로또", "메뉴", "고양이"]
+      buttons: ["흡연/금연구역보러가기", "흡연구역등록", "단속구역등록","사용법보기","문의하기"]
     }
-    render json: @keyboard #json으로 변환
+
+    render json: @keyboard
+
   end
   
   def message
     @user_msg = params[:content]
-    
     @text = "기본 텍스트"
-    if @user_msg == "로또"
-     @text = "행운의 번호는  " + (1..45).to_a.sample(6).sort.to_s
-    elsif @user_msg == "메뉴"
-     @text = "메뉴는  " + ["치킨","피자","햄버거"].sample()
-    elsif @user_msg == "고양이"
-     @url = "http://thecatapi.com/api/images/get?format=xml&type=jpg" 
-     @cat_xml = RestClient.get(@url)
-     @cat_doc = Nokogiri::XML(@cat_xml)
-     @cat_url = @cat_doc.xpath("//url").text
-     @text =@cat_url
+    
+    if @user_msg == "흡연/금연구역보러가기"
+      @text = "흡연/금연구역보러가기를 원하기군요 밑에 버튼은 눌러주세요"
+      @url = "https://mysecond-app-rb2978.c9users.io"
+    elsif @user_msg == "흡연구역등록"
+      @text = "흡연구역등록를 원하기군요 밑에 버튼은 눌러주세요"
+      @url = "https://mysecond-app-rb2978.c9users.io"
+    elsif @user_msg == "단속구역등록"
+      @text = "단속구역등록를 원하기군요 밑에 버튼은 눌러주세요"
+      @url = "https://mysecond-app-rb2978.c9users.io"
+    elsif @user_msg == "사용법보기"
+      @text = "사용법보기를 원하기군요 밑에 버튼은 눌러주세요"
+      @url = "https://mysecond-app-rb2978.c9users.io"
+    elsif @user_msg == "문의하기"
+      @text = "문의하기를 원하기군요 밑에 버튼은 눌러주세요"
+      @url = "https://mysecond-app-rb2978.c9users.io"
     end
    
     @return_msg = {
-      :text => @text
+      
+    "text": @text,
+    "photo": {
+    "url": "https://hello.photo.src",
+    "width": 640,
+    "height": 480
+      },
+      "message_button": {
+      "label": "링크클릭",
+      "url": @url
+      }
+
     }
     
-    @return_msg_photo = {
-      :text => "나만 고양이 없어",
-      :photo => {
-        :url => @text,
-        :width => 720, 
-        :height => 630
-      }
-      
-    }
    
     @return_keyboard = {
       :type => "buttons",
-      buttons: ["로또", "메뉴", "고양이"]
+      buttons: ["흡연/금연구역보러가기", "흡연구역등록", "단속구역등록","사용법보기","문의하기"]
     }
    
-   if @user_msg == "고양이"
-    @result = {
-      :message => @return_msg_photo,
-      :keyboard => @return_keyboard
-    }
-   else
+
     @result = {
       :message => @return_msg,
       :keyboard => @return_keyboard
     }
-   end
+
    
 
     render json: @result
